7185833f8bfaba968e2ff505911ed7faf18d9ff5
Andrei Benea <andrei.benea89@gmail.com>
2025-12-14 00:09:32 -0500

    complate crud budgets, transactions api

--- app.py
+++ app.py
@@ -53,12 +53,14 @@ def budget_page(budget_id):
 
 @app.route("/api/transactions", methods=["GET"])
 def api_transactions():
+    """Get all transactions"""
     transactions = Transaction.query.all()
     return {'transactions': [t.to_dict() for t in transactions]}
 
 
 @app.route("/api/transactions/new", methods=["POST"])
 def api_transactions_new():
+    """Create a new transaction"""
     amount = request.form.get("amount")
     category = request.form.get("category")
     kind = request.form.get("kind")
@@ -74,8 +76,31 @@ def api_transactions_new():
     return {'transactions': [t.to_dict() for t in transactions]}
 
 
+@app.route("/api/transactions/<int:transaction_id>", methods=["GET", "PUT", "DELETE"])
+def api_transaction_details(transaction_id):
+    """Get, update or delete a transaction"""
+    transaction = Transaction.query.get(transaction_id)
+    if transaction is None:
+        return 404
+    if request.method == 'GET':
+        return transaction.to_dict()
+    elif request.method == 'DELETE':
+        db.session.delete(transaction)
+        db.session.commit()
+        return api_transactions()
+    elif request.method == 'PUT':
+        transaction.amount = request.form.get("amount")
+        transaction.category = request.form.get("category")
+        transaction.kind = request.form.get("kind")
+        transaction.notes = request.form.get("notes")
+        db.session.add(transaction)
+        db.session.commit()
+        return api_transactions()
+
+
 @app.route("/api/budgets", methods=["GET"])
 def api_budgets():
+    """Get all budgets"""
     budgets = Budget.query.all()
     return {
         'budgets': [budget.to_dict() for budget in budgets]
@@ -84,6 +109,7 @@ def api_budgets():
 
 @app.route("/api/budgets/new", methods=["POST"])
 def api_budget_new():
+    """Create a new budget"""
     name = request.form.get("name")
     limit = request.form.get("limit")
     categories_string = request.form.get("categories")
@@ -98,6 +124,32 @@ def api_budget_new():
     db.session.commit()
     budgets = Budget.query.all()
     return {'budgets': [b.to_dict() for b in budgets]}
+
+
+@app.route("/api/budgets/<int:budget_id>", methods=["GET", "PUT", "DELETE"])
+def api_budget_details(budget_id):
+    """Get, update or delete a budget"""
+    budget = Budget.query.get(budget_id)
+    if budget is None:
+        return 404
+    if request.method == 'GET':
+        return budget.to_dict()
+    elif request.method == 'DELETE':
+        db.session.delete(budget)
+        db.session.commit()
+        return api_budgets()
+    elif request.method == 'PUT':
+        budget.name = request.form.get("name")
+        budget.limit = request.form.get("limit")
+        budget.categories = []
+        for cat in request.form.get("categories").split(","):
+            category = Category()
+            category.name = cat.strip()
+            budget.categories.append(category)
+
+        db.session.add(budget)
+        db.session.commit()
+        return budget.to_dict()
 
 
 if __name__ == "__main__":
